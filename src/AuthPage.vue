40e3dd07196dc0f5081eee6b0641ff7f2c9def87
student1 <student1@pcoip.ki.agh.edu.pl>
2022-05-28 12:09:53 +0200

    create new component

---
+++ src/AuthPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div> Witaj {{userNameToDisplay}} </div>
+    <button @click="logOut()">Wyloguj</button>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['email'],
+
+  data() {
+    return {
+    }
+  },
+
+  computed: {
+    userNameToDisplay() {
+      return this.email || 'Nieznany użytkownik';
+    }
+  },
+
+  methods: {
+    logOut() {
+      this.$emit('logout');
+    },
+
+  }
+}
+</script>
